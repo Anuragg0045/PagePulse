06c7c47e592dcf6506fad195c3949d238d04bb04
Shivam Sharma <anuragg0045@gmail.com>
2026-07-26 17:12:15 +0530

    Use dynamic PORT for deployment

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
-const app=require("./app");
-app.listen(3000,()=>{
-    console.log("Server is running on tha port 3000");
-})
+const app = require("./app");
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
